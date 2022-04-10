b5f9502ac5e9bc26e1bf6eba695ada07177de951
lvwd <930502410@qq.com>
2022-04-11 00:35:56 +0800

    add all files

---
+++ src/com/yhzy/Test01.java
@@ -0,0 +1,33 @@
+package com.yhzy;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test01 extends HttpServlet {
+
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("你好");
+		out.println("吃了吗");
+		out.println("在干什么???");
+	
+		out.flush();
+		out.close();
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
